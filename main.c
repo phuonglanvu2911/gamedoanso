11a4de4aea320ca75dc6796c945dd8b08a6adb41
Phương Lan <33753057+phuonglanvu2911@users.noreply.github.com>
2017-11-21 22:37:26 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -2,41 +2,50 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 int main()
 {
     srand((unsigned int) time(0));
     int a,b,count, c, d;   // a và b là khoảng, count là biến đếm, c là số mình đoán, d là số ngẫu nhiên
-    count=8;
-    printf("nhap so a: ");
-    scanf("%d", &a);
-    printf("nhap so b: ");
-    scanf("%d", &b);
-    d=a + rand()%(b-a+1);
+    count=1;
+    printf("\t\tCHAO MUNG BAN DEN VOI GAME DOAN SO \n");
+    printf("\nCHU Y a > b \n");
 
-    do
-    {
+                        do //do để khai báo a và b a>b
+                        {
+                            printf("nhap so a: ");
+                            scanf("%d", &a);
+                            printf("nhap so b: ");
+                            scanf("%d", &b);
+                            printf("ban co 8 lan doan\n\n\n");
+                            d=a + rand()%(b-a+1);
+                        }
+                        while (a>=b); // while để có lặp lại trò chơi game hay không
 
-        printf("\nlan doan thu %d :", count);
-        printf("\nso ban doan la:");
-        scanf("%d", &c);
-        if (c==d)
-        {
-            printf("congratulation\n");
-            break;
-        }
 
-        else if (count==1)
-        {
-            printf("YOU LOSE !!!\n");
-            printf("%d", d);
-        }
+                        do // do để xuất kết quả xem đúng hay sai
+                        {
+                            printf("\n\tlan doan thu %d ", count);
+                            printf("\nso ban doan la:");
+                            scanf("%d", &c);
+                            if (c==d)
+                            {
+                                printf("congratulation\n");
+                                break;
+                            }
 
-        else if (d>c) printf("higher\n");
-        else printf("lower\n");
-        count--;
-    }
+                            else if (count==8)
+                            {
+                                printf("YOU LOSE !!!\n");
+                                printf("%d", d);
+                            }
 
-    while (count!=0);
+                            else if (d>c) printf("higher\n");
+                            else printf("lower\n");
+                            count++;
+                        }
+                        while (count<=8);// while để xuất kết quả xem đúng hay sai
+
     return 0;
 }
 
